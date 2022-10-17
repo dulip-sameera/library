2c6d5d574064e160f268a990fcf7f8e030b0cc3d
dulip-sameera <dsgitrepo101@gmail.com>
2022-10-17 09:35:49 +0530

    add delete function to library

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ function Library() {
 }
 
 Library.prototype = {
+  //add a book to the library
   add(title, author, noOfPages, readStatus) {
     this.data[this.data.length] = new Book(
       title,
@@ -54,10 +55,28 @@ Library.prototype = {
     );
   },
 
+  // return all books in the library
   getAllBooks() {
     return this.data;
   },
+
+  //delete a book from library
+  delete(id) {
+    this.data.splice(id, 1);
+  },
 };
 
+// testing..
+
 const myLibrary = new Library();
-console.table();
+myLibrary.add("ad", "as", "232", true);
+myLibrary.add("aasdd", "aasds", "22", false);
+myLibrary.add("ads", "asfg", "222", true);
+myLibrary.add("adhnf", "adaws", "432", true);
+myLibrary.add("adfvsfhd", "dsas", "209", false);
+
+console.table(myLibrary.getAllBooks());
+
+myLibrary.delete(2);
+
+console.table(myLibrary.getAllBooks());
